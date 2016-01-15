dfab52e0de8fa622e06f3865e1ee43b2ec599c71
Zagora <zagorakom@gmail.com>
2016-01-15 23:06:00 +0100

    Patch-v1.1

--- sendmail.php
+++ sendmail.php
@@ -3,7 +3,7 @@
 $to      = 'zagorakom@gmail.com' . ', ';
 $to     .= 'ask@devmuerto.com';
 $subject = 'Offer from DevMuerto';
-$message = "---- New Offer from DevMuerto: ---- \n\nName: $_POST[q1] \nDetails: $_POST[q2] \nE-mail: $_POST[q3] \nComment: $_POST[q4]";
+$message = "NEW OFFER FROM DEVMUERTO: \n\nName: $_POST[q1] \nDetails: $_POST[q2] \nE-mail: $_POST[q3] \nComment: $_POST[q4]";
 $message = wordwrap($message, 70, "\r\n");
 $headers = 'From: offer@devmuerto.com' . "\r\n" .
 			'Reply-To: noreply@devmuerto.com' . "\r\n" .
